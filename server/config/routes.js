2d86f8afaf2bef709457c31a482dd6e7585b76fa
drumev <milan_drumev@yahoo.com>
2014-10-07 21:22:59 +0300

    Added all basic routes for create and request users, offers and categories TODO: replace the generic function used with a controller callback

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,4 +3,90 @@ module.exports = function(app) {
         res.render('index');
     });
 
+    /* User-related routes START*/
+
+    //Get all users
+    app.get('/user', function(req, res) {
+        res.render('index');
+    });
+
+    // Get user by ID
+    app.get('/user/:id', function(req, res) {
+        res.render('index');
+    });
+
+    // Register new user
+    app.post('/user', function(req, res) {
+        res.render('index');
+    });
+
+    /*    User-related routes END   */
+    /********************************/
+    /* Category-related routes START*/
+
+    // Get all categories
+    app.get('/category', function(req, res) {
+        res.render('index');
+    });
+
+    // Get category by ID
+    app.get('/category/:id', function(req, res) {
+        res.render('index');
+    });
+
+    // Create new Category
+    app.post('/user', function(req, res) {
+        res.render('index');
+    });
+    /* Category-related routes END*/
+    /******************************/
+    /* Offer-related routes Start*/
+
+    // Get all Offers
+    app.get('/offer', function(req, res) {
+        res.render('index');
+    });
+
+    // Get Offer by ID
+    app.get('/offer/:id', function(req, res) {
+        res.render('index');
+    });
+
+    // Get all Offers by USER ID
+    app.get('/offer/:userId', function(req, res) {
+        res.render('index');
+    });
+
+    // Get all Offers by Category ID
+    app.get('/offer/:categoryId', function(req, res) {
+        res.render('index');
+    });
+
+    // Get latest Offers
+    app.get('/offer/latest', function(req, res) {
+        res.render('index');
+    });
+
+    // Get latest Offers by category
+    app.get('/offer/latest/:categoryId', function(req, res) {
+        res.render('index');
+    });
+
+    //Search Offers by title key word => TODO: receive an array of keywords
+    app.get('/offer/:titleKeyWord', function(req, res) {
+        res.render('index');
+    });
+
+    //Search Offers by description key word => TODO: receive an array of keywords
+    app.get('/offer/latest/:descriptionKeyWord', function(req, res) {
+        res.render('index');
+    });
+
+    // Create new Offer
+    app.post('/offer', function(req, res) {
+        res.render('index');
+    });
+    /* Offer-related routes END*/
+    /******************************/
+    /* Offer-related routes Start*/
 };
