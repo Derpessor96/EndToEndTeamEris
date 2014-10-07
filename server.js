f5a16adf5d37c67bb0c02ce3763ae6235ce49738
drumev <milan_drumev@yahoo.com>
2014-10-07 21:02:40 +0300

    First route added Jade index template modified

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var express = require('express');
 var app = express(),
     serverConfig = require('./server/config/config')['dev'];
 
-
 require('./server/config/express')(app, serverConfig);
-require('./server/config/mongoose')(serverConfig);
 
 app.listen(serverConfig._PORT);
-console.log("Server running on port: " + serverCofig._PORT);
+console.log("Server running on port: " + serverConfig._PORT);
