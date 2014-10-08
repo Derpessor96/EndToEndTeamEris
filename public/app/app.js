8a6131bbd6bc875771f11c783163020d638980b1
dpesheva <dkshosa@gmail.com>
2014-10-08 17:48:54 +0300

    UI updates

--- public/app/app.js
+++ public/app/app.js
@@ -1,35 +1,40 @@
-var app = angular.module('app', ['ngResource', 'ngRoute']);
+var app = angular.module( 'app', [ 'ngResource', 'ngRoute' ] );
 
-app.config(function ($routeProvider, $locationProvider) {
+app.config( function ( $routeProvider, $locationProvider ) {
 
     var routeUserChecks = {
         adminRole: {
-            authenticate: function (auth) {
-                return auth.isAuthorizedForRole('admin');
+            authenticate: function ( auth ) {
+                return auth.isAuthorizedForRole( 'admin' );
             }
         },
         authenticated: {
-            authenticate: function (auth) {
+            authenticate: function ( auth ) {
                 return auth.isAuthenticated();
             }
         }
     };
 
     $routeProvider
-        .when('/', {
-            templateUrl: 'app/partials/main/home.html', controller: 'MainCtrl'
-        })
-        .when('/signup', {
+        .when( '/', {
+            templateUrl: 'app/partials/main/home.html',
+            controller: 'MainCtrl'
+        } )
+        .when( '/latest-ads', {
+            templateUrl: 'app/partials/main/home.html',
+            controller: 'MainCtrl'
+        } )
+        .when( '/signup', {
             templateUrl: './app/account/signup.html',
             controller: 'SignUpCtrl'
-        })
-})
-    .constant('baseServiceUrl', 'http://localhost:7777/');
+        } )
+} )
+    .constant( 'baseServiceUrl', 'http://localhost:7777/' );
 
-app.run(function ($rootScope, $location) {
-    $rootScope.$on('$routeChangeError', function (ev, current, previous, rejection) {
-        if (rejection === 'not authorized') {
-            $location.path('/');
+app.run( function ( $rootScope, $location ) {
+    $rootScope.$on( '$routeChangeError', function ( ev, current, previous, rejection ) {
+        if ( rejection === 'not authorized' ) {
+            $location.path( '/' );
         }
-    })
-});
+    } )
+} );
