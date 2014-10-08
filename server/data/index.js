21400af86e8e2224590a18c25a132c997c0d9afc
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-10-08 19:55:53 +0300

    data layer

---
+++ server/data/index.js
@@ -0,0 +1,12 @@
+var categoriesData = require('./categoriesData'),
+    offersData = require('./offersData'),
+    usersData = require('./usersData'),
+    salesData = require('./salesData');
+
+
+module.exports = {
+    categories : categoriesData,
+    offers : offersData,
+    users: usersData,
+    sales : salesData
+};
