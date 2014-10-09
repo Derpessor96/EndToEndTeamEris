ee294ce732c198ad11b2e55d3e19deb8f75bb014
Derpessor96 <ivan.tonchev96@gmail.com>
2014-10-10 00:32:06 +0300

    Added a new route to the angular draft.

--- public/js/app.js
+++ public/js/app.js
@@ -1,71 +1,75 @@
-var app = angular.module( 'app', [ 'ngResource', 'ngRoute' ] );
+var app = angular.module('app', ['ngResource', 'ngRoute']);
 
-app.config( function ( $routeProvider, $locationProvider ) {
+app.config(function ($routeProvider, $locationProvider) {
 
-    var routeUserChecks = {
-        adminRole: {
-            authenticate: function ( auth ) {
-                return auth.isAuthorizedForRole( 'admin' );
-            }
-        },
-        authenticated: {
-            authenticate: function ( auth ) {
-                return auth.isAuthenticated();
-            }
-        }
-    };
+	var routeUserChecks = {
+		adminRole: {
+			authenticate: function (auth) {
+				return auth.isAuthorizedForRole('admin');
+			}
+		},
+		authenticated: {
+			authenticate: function (auth) {
+				return auth.isAuthenticated();
+			}
+		}
+	};
 
-    $routeProvider
-        .when( '/', {
-            templateUrl: 'partials/main/home',
+	$routeProvider
+        .when('/', {
+        	templateUrl: 'partials/main/home',
+        	controller: 'MainCtrl'
+        })
+        .when('/users', {
+        	templateUrl: 'partials/users/users',
+        	controller: 'MainCtrl'
+        })
+        .when('/users/:id', {
+        	templateUrl: 'partials/users/userDetails',
+        	controller: 'MainCtrl'
+        })
+        .when('/users/:id/offers', {
+        	templateUrl: 'partials/users/userOffers',
+        	controller: 'MainCtrl'
+        })
+        .when('/users/:id/sales', {
+        	templateUrl: 'partials/users/userSales',
+        	controller: 'MainCtrl'
+        })
+        .when('/offers', {
+        	templateUrl: 'partials/offers/offers',
+        	controller: 'MainCtrl'
+        })
+        .when('/offers/:id', {
+        	templateUrl: 'partials/offers/offerDetails',
+        	controller: 'MainCtrl'
+        })
+        .when('/offers/create', {
+        	templateUrl: 'partials/offers/createOffer',
+        	controller: 'MainCtrl'
+        })
+        .when('/signup', {
+        	templateUrl: 'partials/main/signup',
+        	controller: 'SignUpCtrl'
+        })
+        .when('/categories/:id/offers', {
+        	templateUrl: '', // TODO: add partial
+        	controller: 'SignUpCtrl'
+        })
+        .when('/categories', {
+        	templateUrl: '', // TODO: add partial
+        	controller: 'SignUpCtrl'
+        })
+        .otherwise({
+        	redirectTo: '/'
+        });
+})
+    .constant('baseServiceUrl', 'http://localhost:7777/');
 
-            controller: 'MainCtrl'
-        } )
-       //.when('/categories', {
-       //    templateUrl: 'views/partials/trip-create.html',
-       //    controller: 'TripCreateCtrl'
-        .when( '/offers', {
-            templateUrl: 'partials/offers/offers',
-            controller: 'MainCtrl'
-        } )
-        .when( '/offers/create', {
-            templateUrl: 'partials/offers/createOffer',
-            controller: 'MainCtrl'
-        } )
-        .when( '/offers/:id', {
-            templateUrl: 'partials/offers/offerDetails',
-            controller: 'MainCtrl'
-        } )
-        .when( '/users', {
-            templateUrl: 'partials/users/users',
-            controller: 'MainCtrl'
-        } )
-        .when( '/users/:id', {
-            templateUrl: 'partials/users/userDetails',
-            controller: 'MainCtrl'
-        } )
-        .when( '/users/:id/sales', {
-            templateUrl: 'partials/users/userSales',
-            controller: 'MainCtrl'
-        } )
-        .when( '/users/:id/offers', {
-            templateUrl: 'partials/users/userOffers',
-            controller: 'MainCtrl'
-        } )
-        .when( '/signup', {
-            templateUrl: 'partials/main/signup',
-            controller: 'SignUpCtrl'
-        } )
-        .otherwise( {
-            redirectTo: '/'
-        } );
-} )
-    .constant( 'baseServiceUrl', 'http://localhost:7777/' );
-
-app.run( function ( $rootScope, $location ) {
-    $rootScope.$on( '$routeChangeError', function ( ev, current, previous, rejection ) {
-        if ( rejection === 'not authorized' ) {
-            $location.path( '/' );
-        }
-    } )
-} );
+app.run(function ($rootScope, $location) {
+	$rootScope.$on('$routeChangeError', function (ev, current, previous, rejection) {
+		if (rejection === 'not authorized') {
+			$location.path('/');
+		}
+	})
+});
